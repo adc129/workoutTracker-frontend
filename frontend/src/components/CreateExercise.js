352b84093a55c0e17541ff1dd3d81831cfbedfb2
aaron <>
2021-01-07 17:29:34 -0600

    moved frontend to firebase
    heroku was too slow :(

---
+++ frontend/src/components/CreateExercise.js
@@ -0,0 +1,178 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+export default class createExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeCaloriesBurned = this.onChangeCaloriesBurned.bind(this);
+    this.onChangeCaloriesEaten = this.onChangeCaloriesEaten.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      caloriesBurned:0,
+      caloriesEaten:0,
+      date: new Date(),
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://workoutbackend2021.herokuapp.com/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          })
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    })
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    })
+  }
+  onChangeCaloriesBurned(e) {
+    this.setState({
+      caloriesBurned: e.target.value
+    })
+  }
+  onChangeCaloriesEaten(e) {
+    this.setState({
+      caloriesEaten: e.target.value
+    })
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      caloriesBurned: this.state.caloriesBurned,
+      caloriesEaten: this.state.caloriesEaten,
+      date: this.state.date,
+    }
+
+    console.log(exercise);
+
+    axios.post('https://workoutbackend2021.herokuapp.com/exercises/add', exercise)
+      .then(res => console.log(res.data));
+
+    window.location = '/';
+  }
+
+  render() {
+    return (
+    <div>
+      <h3>Create New Exercise Log</h3>
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}>
+              {
+                this.state.users.map(function(user) {
+                  return <option
+                    key={user}
+                    value={user}>{user}
+                    </option>;
+                })
+              }
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+              />
+        </div>
+
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+              />
+        </div>
+
+        <div className="form-group">
+          <label>Calories Burned: </label>
+          <input
+              type="text"
+              className="form-control"
+              value={this.state.caloriesBurned}
+              onChange={this.onChangeCaloriesBurned}
+              />
+        </div>
+
+        <div className="form-group">
+          <label>Calories Ate: </label>
+          <input
+              type="text"
+              className="form-control"
+              value={this.state.caloriesEaten}
+              onChange={this.onChangeCaloriesEaten}
+              />
+        </div>
+
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={this.state.date}
+              onChange={this.onChangeDate}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+    )
+  }
+}
